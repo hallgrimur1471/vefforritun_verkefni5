4fd3741a2f0337b1d0cf49ce1406f01bb312df96
hallgrimur1471 <hallgrimur1471@gmail.com>
2016-11-06 18:11:17 +0000

    using callback

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const fs = require('fs');
 
 var multiplier = require('../src/multiplier.js')
 
@@ -15,36 +16,30 @@ router.post('/', function(req, res, next) {
   console.log(textboxInput);
   console.log(multiplier.multiply([4, 4]));
 
-  calculations(textboxInput)
-    .then((result) => {
+  calculations(textboxInput, (result) => {
       res.render('results', {
         title: 'Niðurstöður margföldunnar',
-        input: '1 2 3 a 4',
-        foundNumbers: '1 2 3 4',
-        multiplicationResults: '24',
-        factors: '1 2 3 4 6 8 12 24'
+        input: result.textboxInput,
+        foundNumbers: result.foundNumbers,
+        multiplicationResults: result.multiplicationResults,
+        factors: result.factors
       });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.render('error', {title: 'Oh no!', error});
     });
 });
 
-function calculations(textboxInput) {
+function calculations(textboxInput, callback) {
   const numbers = multiplier.splitIntoNumbers(textboxInput);
   const mult = multiplier.multiply(numbers);
   const factors = multiplier.factorize(mult);
 
   var calcResults = {
-    input: textboxInput,
-    foundNumbersText: foundNumbersText,
+    textboxInput,
     foundNumbers: numbers.toString(),
     multiplicationResults: mult.toString(),
     factors: factors.toString(),
-  }
+  };
 
-  return calcResults;
+  callback(calcResults);
 }
 
 module.exports = router;
