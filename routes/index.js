1ba1dfd84bd077187c52633548d7d6d8f4199296
hallgrimur1471 <hallgrimur1471@gmail.com>
2016-11-06 15:54:40 +0000

    button press sends GET request through a script in the html

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello world' });
+  res.render('index', { title: 'Margföldunarvélin' });
+});
+
+/* GET results. */
+router.post('/results', function(req, res, next) {
+  res.render('index', { title: 'Hello world2' });
 });
 
 module.exports = router;
